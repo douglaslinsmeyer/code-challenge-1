509bec7cbfd76ff3e36da3ecab84cb02938dd22a
Jared Dubina <jaredd@ping.com>
2019-11-06 20:33:23 -0700

    just some return earlies

--- app.js
+++ app.js
@@ -6,4 +6,16 @@ const myArgs = process.argv.slice(2);
 
 console.log(myArgs);
 
+if (myArgs[0] === undefined) {
+    throw 'To start the game, please use -f hands.txt';
+}
+
+if (myArgs[0] !== '-f') {
+    throw `${myArgs[0]} is an unknown function. Please use -f hands.txt`;
+}
+
+if (myArgs[1] !== 'hands.txt') {
+    throw `${myArgs[1]} is an unkown function. Please use -f hands.txt`;
+}
+
 questions.confirmUserWantsToPlay(start.game.bind(this, myArgs));
--- src/PokerFunction.js
+++ src/PokerFunction.js
@@ -2,6 +2,17 @@ class PokerFunction {
     letsBegin(playersArr, myArgs) {
         console.log(playersArr);
         console.log(myArgs);
+
+        if (myArgs[0] !== '-f') {
+            return console.log(`${myArgs[0]} is a known function. Please use -f`);
+        }
+
+        if (myArgs[1] !== 'hands.txt') {
+            return console.log(`${myArgs[1]} is an unkown function. Please use hands.txt`);
+        }
+
+        
+
     }
 }
 
