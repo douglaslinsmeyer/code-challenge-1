0bcfbd38b42c070d30b4ce498713662377f65b8a
Jared Dubina <jaredd@ping.com>
2019-11-06 20:38:24 -0700

    revised return earlies

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const myArgs = process.argv.slice(2);
 console.log(myArgs);
 
 if (myArgs[0] === undefined) {
-    throw 'To start the game, please use -f hands.txt';
+    throw 'To start the game, please use "-f hands.txt" or "-h with 10 cards"';
 }
 
-if (myArgs[0] !== '-f') {
-    throw `${myArgs[0]} is an unknown function. Please use -f hands.txt`;
+if (myArgs[0] !== '-f' || myArgs[0] !== '-h') {
+    throw `${myArgs[0]} is an unknown function. Please use "-f hands.txt" or "-h" with 10 cards`;
 }
 
-if (myArgs[1] !== 'hands.txt') {
-    throw `${myArgs[1]} is an unkown function. Please use -f hands.txt`;
+if (myArgs[0] !== '-f' && myArgs[1] !== 'hands.txt') {
+    throw `${myArgs[1]} is an unkown file. Please use "-f hands.txt" or "-h with 10 cards"`;
 }
 
 questions.confirmUserWantsToPlay(start.game.bind(this, myArgs));
